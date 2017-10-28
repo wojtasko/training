6b0664f9bd272d93d56f69fb4f8151e5a1e70dfd
wojtasko <ricomaz@gmail.com>
2017-10-28 23:09:42 +0200

    Initial commit with maven conf and spring boot

---
+++ src/main/java/com/wojtasko/example/Example/first/ExampleFirstApplication.java
@@ -0,0 +1,12 @@
+package com.wojtasko.example.Example.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleFirstApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wojtasko/example/Example/first/ExampleFirstApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wojtasko.example.Example.first;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleFirstApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
